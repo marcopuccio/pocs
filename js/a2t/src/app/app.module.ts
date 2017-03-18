f9df1a05bd2090e7dbadde43c4755930aad459ad
Marco Puccio <mpuccio90@gmail.com>
2017-03-17 22:27:41 -0300

    routing

--- js/a2t/src/app/app.module.ts
+++ js/a2t/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -9,27 +9,13 @@ import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
-const ROUTES = [
-    {
-      path: '',
-      redirectTo: '/dashboard',
-      pathMatch: 'full'
-    },
-    {
-      path: 'dashboard',
-      component: DashboardComponent
-    },
-    {
-      path: 'heroes',
-      component: HeroesComponent
-    },
-]
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(ROUTES),
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
