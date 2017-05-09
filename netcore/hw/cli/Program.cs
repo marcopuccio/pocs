bdc0bf61b3896fe34c4e7133c91de1fb05ac2b39
Marco Puccio <mpuccio90@gmail.com>
2017-05-08 21:27:58 -0300

    hello

---
+++ netcore/hw/cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using utils;
+
+namespace cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string[] displayString = StringArrayMaker.Splitify("Hello World!");
+
+            foreach(String s in displayString) 
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ netcore/hw/utils/StringArrayMaker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace utils
+{
+    public class StringArrayMaker
+    {
+        public static String[] Splitify(String toSplitString)
+        {   
+            return toSplitString.Split();
+        }
+    }
+}
